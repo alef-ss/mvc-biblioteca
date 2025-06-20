eef16ada79e617efc9ec31ba9a79a9940d269df4
alef-ss <alef.ss@pronton.me>
2025-06-20 09:45:05 -0300

    refazendo o painel

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -1,27 +1,32 @@
 <?php
 session_start();
 
+// Scriptzin pra ver se o professor tá logado, o '!' significa 'não', então se ele não estiver logado, pede pra ele fazer login
 if (!isset($_SESSION['professor_id'])) {
     header("Location: ../frontend/login.php");
     exit();
 }
 
-// Database connection
+// Incluir o arquivo pra fazer a conexão com o banco de dados
 include('../includes/conn.php');
 
-// Query to get the total number of students
-$result_students = $conn->query("SELECT COUNT(*) as total FROM alunos");
-$total_students = $result_students->fetch_assoc()['total'];
+// Consulta pra pegar o número total de alunos
+$resultadoAlunos = $conn->query("SELECT COUNT(*) as total FROM alunos");
+$totalAlunos = $resultadoAlunos->fetch_assoc()['total'];
 
-// Query to get the total number of books
-$result_books = $conn->query("SELECT COUNT(*) as total FROM livros");
-$total_books = $result_books->fetch_assoc()['total'];
+// Consulta pra pegar o número total de livros
+$resultadoLivros = $conn->query("SELECT COUNT(*) as total FROM livros");
+$totalLivros = $resultadoLivros->fetch_assoc()['total'];
 
-// Query to get the total number of active loans
-$result_loans = $conn->query("SELECT COUNT(*) as total FROM emprestimos WHERE devolvido = '0'");
-$total_loans = $result_loans->fetch_assoc()['total'];
+// Consulta pra pegar o número total de empréstimos
+$resultadoEmprestimos = $conn->query("SELECT COUNT(*) as total FROM emprestimos WHERE devolvido = '0'");
+$totalEmprestimos = $resultadoEmprestimos->fetch_assoc()['total'];
 
-// Query to get the total number of pending returns
-$result_pending_returns = $conn->query("SELECT COUNT(*) as total FROM emprestimos WHERE data_devolucao IS NULL");
-$total_pending_returns = $result_pending_returns->fetch_assoc()['total'];
+// Consulta pra pegar o número total de empréstimos pendentes
+$resultadoDevolucaoPendente = $conn->query("SELECT COUNT(*) as total FROM emprestimos WHERE data_devolucao IS NULL");
+$totalDevolucoesPendentes = $resultadoDevolucaoPendente->fetch_assoc()['total'];
+
+
+$salas = $conn->query("SELECT COUNT(*) as total FROM alunos WHERE serie");
+$totalSalas = $salas->fetch_assoc()['total'];
 ?>
