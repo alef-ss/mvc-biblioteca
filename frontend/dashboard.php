518926cf2ac918a501c81402cc51a5191c079103
HablasBlaBla <00001111987208sp@al.educacao.sp.gov.br>
2025-08-21 07:23:36 -0300

    Revert "Merge branch 'main' of https://github.com/HablasBlaBla/mvc-biblioteca"
    This reverts commit ad5856445834b7e671751749868e9ddf7f2a86c6, reversing
    changes made to 19f38faf9b9b630bda443c5875cec15c6f8b8380.

--- frontend/dashboard.php
+++ frontend/dashboard.php
@@ -1,15 +1,19 @@
 <?php
 include('../backend/dashboard.php');
-// Chama a função para obter os 10 livros mais emprestados
-$livrosMaisEmprestados = obterLivrosMaisEmprestados($conn);
 ?>
 
 <!DOCTYPE html>
+<html lang="pt" data-theme="light">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <title>Painel - Sistema de Gestão de Biblioteca</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <link rel="icon" href="favicon/favicon-32x32.png" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/dashboard.css">
+  <!-- <link rel="stylesheet" href="assets/css/global_colors.css"> -->
 </head>
-
 <body>
