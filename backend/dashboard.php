65c4380e890b5c8a261e0a6ea731f2e67b70e981
HablasBlaBla <00001111987208sp@al.educacao.sp.gov.br>
2025-08-26 11:07:37 -0300

    fiz algumas alterações que tinha perdido

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -11,17 +11,17 @@ include('../includes/conn.php');
 
 // Query to get the total number of students
 $result_students = $conn->query("SELECT COUNT(*) as total FROM alunos");
-$total_students = $result_students->fetch_assoc()['total'];
+$totalAlunos = $result_students->fetch_assoc()['total'];
 
 // Query to get the total number of books
 $result_books = $conn->query("SELECT COUNT(*) as total FROM livros");
-$total_books = $result_books->fetch_assoc()['total'];
+$totalLivros = $result_books->fetch_assoc()['total'];
 
 // Query to get the total number of active loans
 $result_loans = $conn->query("SELECT COUNT(*) as total FROM emprestimos WHERE devolvido = '0'");
-$total_loans = $result_loans->fetch_assoc()['total'];
+$totalEmprestimos = $result_loans->fetch_assoc()['total'];
 
 // Query to get the total number of pending returns
 $result_pending_returns = $conn->query("SELECT COUNT(*) as total FROM emprestimos WHERE data_devolucao IS NULL");
-$total_pending_returns = $result_pending_returns->fetch_assoc()['total'];
+$totalDevolucoesPendentes = $result_pending_returns->fetch_assoc()['total'];
 ?>
